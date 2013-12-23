5867607a1e61219adc3304b6b6a4284b5e9777ae
zhoujd <zjd-405@163.com>
2013-12-23 23:10:32 +0800

    update for ipython

--- bin/ipython.bat
+++ bin/ipython.bat
@@ -1,1 +1,2 @@
-@C:\Python27\python.exe -i C:\Python27\scripts\ipython-script.py %*
+@if exist C:\Python27\scripts\ipython-script.py C:\Python27\python.exe -i C:\Python27\scripts\ipython-script.py %*
+@if exist C:\Python27\scripts\ipython C:\Python27\python.exe -i C:\Python27\scripts\ipython %*
