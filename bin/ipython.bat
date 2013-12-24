99a9cdc34714f0e52ffc4cf84fb7162391c088f4
zhoujd <zjd-405@163.com>
2013-12-24 09:51:07 +0800

    update script for ipython on win7

--- bin/ipython.bat
+++ bin/ipython.bat
@@ -1,2 +1,19 @@
-@if exist C:\Python27\scripts\ipython-script.py C:\Python27\python.exe -i C:\Python27\scripts\ipython-script.py %*
-@if exist C:\Python27\scripts\ipython C:\Python27\python.exe -i C:\Python27\scripts\ipython %*
+@echo off
+
+rem This is for emacs run IPython
+rem Set env var before run this script
+set PYTHON_HOME="C:\Python27"
+
+if exist %PYTHON_HOME%\scripts\ipython-script.py goto 1
+if exist %PYTHON_HOME%\scripts\ipython goto 2
+
+:1
+%PYTHON_HOME%\python.exe -i %PYTHON_HOME%\scripts\ipython-script.py %*
+goto END
+:2
+%PYTHON_HOME%\python.exe -i %PYTHON_HOME%\scripts\ipython %*
+goto END
+:END
+
+@echo on
+
