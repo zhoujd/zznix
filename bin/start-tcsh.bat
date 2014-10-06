efb68536c7d34805ca47c26e330ffdf1fc6dbb2c
zhoujd <zjd-405@163.com>
2014-10-07 00:53:42 +0800

    saving

--- bin/start-tcsh.bat
+++ bin/start-tcsh.bat
@@ -1,2 +1,17 @@
-@set HOME=c:/zznix/home/zhoujd
-@c:/zznix/share/tcsh/tcsh-6.18.00-x64.exe -l
+@rem This is for start shell
+
+@set ZZNIX=c:/zznix
+@set HOME=%ZZNIX%/home/zhoujd
+
+@if exist %ZZNIX%/bin/csh.exe goto 1
+@if exist %ZZNIX%/bin/sh.exe goto 2
+
+:1
+@%ZZNIX%/share/tcsh/tcsh-x64.exe -l
+@goto END
+
+:2
+@%ZZNIX%/share/bash/sh --login -i
+@goto END
+
+:END
