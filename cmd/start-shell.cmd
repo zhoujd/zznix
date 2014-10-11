7da6322f8b1ee8e9fbe86ef59db4eb99eceafaaf
zhoujd <zjd-405@163.com>
2014-10-12 01:06:15 +0800

    shell select

--- cmd/start-shell.cmd
+++ cmd/start-shell.cmd
@@ -2,9 +2,10 @@
 
 @set ZZNIX=c:/zznix
 @set HOME=%ZZNIX%/home/zhoujd
+@set SELECT=csh
 
-@if exist %ZZNIX%/bin/csh.exe goto 1
-@if exist %ZZNIX%/bin/sh.exe goto 2
+@if %SELECT% == csh goto 1
+@if %SELECT% == sh goto 2
 
 :1
 @%ZZNIX%/bin/csh.exe -l
