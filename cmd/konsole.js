4e733c0cd303216b6faa5d00f802330792d48eb0
zhoujd <zjd-405@163.com>
2014-12-05 11:27:17 +0800

    add for more remote

--- cmd/konsole.js
+++ cmd/konsole.js
@@ -1,4 +1,4 @@
 //Konsole.js - alternative to the Run Utility example provided above
 //JavaScript to display a remote konsole without a local Command Prompt
 var wshell = new ActiveXObject('WScript.Shell');
-wshell.Run('"C:\\zznix\\share\\putty\\plink.exe" -load x11-forward konsole --ls', 0);
+wshell.Run('"C:\\zznix\\share\\putty\\plink.exe" -load x11-forward gedit', 0, false);
