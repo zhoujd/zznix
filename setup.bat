d7fd3dac6f8d8cd8d8bdbf1905c5d90598919791
zhoujd <zjd-405@163.com>
2017-10-24 14:09:48 +0800

    aving

--- setup.bat
+++ setup.bat
@@ -8,7 +8,7 @@ set CURRENT_DIR=%CD%
 if exist %MYCFG_PATH% del %MYCFG_PATH%
 
 echo ### zznix .bash_profile>> %MYCFG_PATH%
-echo export HOME=c:/zznix/home/zhoujd>> %MYCFG_PATH%
+echo export HOME=%MYHOME%>> %MYCFG_PATH%
 echo . $HOME/.bashrc>> %MYCFG_PATH%
 
 set MYHOME=
