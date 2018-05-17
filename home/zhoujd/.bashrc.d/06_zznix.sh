4573fae418dd333621721316a8a1fa69f2b8e4e2
zhoujd <zjd-405@163.com>
2018-05-17 10:06:39 +0800

    add more for zznix

---
+++ home/zhoujd/.bashrc.d/06_zznix.sh
@@ -0,0 +1,12 @@
+
+zznix()
+{
+    case $1 in 
+        home )
+            cd c:/zznix
+            ;;
+        * )
+            echo "zznix home  : change current folder to c:/zznix"
+            ;;
+    esac
+}
