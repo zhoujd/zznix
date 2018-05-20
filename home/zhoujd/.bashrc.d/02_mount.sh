8b22addc2c6b46d09eb31d29d9f5e27a551e2ad4
zhoujd <zjd-405@163.com>
2018-05-20 11:07:39 +0800

    add more comments

--- home/zhoujd/.bashrc.d/02_mount.sh
+++ home/zhoujd/.bashrc.d/02_mount.sh
@@ -2,3 +2,7 @@
 
 #test -d /zach || mount -f c:/zznix/zach /zach
 #test -d /home || mount -f c:/zznix/home /home
+
+## with mklink on win10
+#mklink /d c:\Git\zach c:\zznix\zach
+#mklink /d c:\Git\home c:\zznix\home
