e08f3209c356f11a0276be8542c45c82b0df83a9
Zachary Zhou <zchrzhou@gmail.com>
2024-08-08 14:39:15 +0800

    Update for bash alias

--- home/zach/.bashrc.d/05-alias.sh
+++ home/zach/.bashrc.d/05-alias.sh
@@ -1,10 +1,21 @@
+## 05-alias.sh
 
 # for chinese file name show
 alias ls='ls --show-control-chars --color=always'
 alias ll='ls -alF'
 alias l='ls -CF'
-alias dir='ls -C'        # only on win7
-alias cls='clear'        # only on win7
+alias la='ls -Al'                  # show hide files
+alias lx='ls -lXB'                 # sort by extend
+alias lk='ls -lSr'                 # sort by size
+alias lc='ls -lcr'                 # sort by time
+alias lu='ls -lur'                 # sort by access time
+alias lr='ls -lR'                  # recurse ls
+alias lt='ls -ltr'                 # sort by date
+alias lh='ls -lh'                  # ls for -h
+alias lm='ls -al | more'           # pipeline to more
+
+alias dir='ls -C'                  # only on win7
+alias cls='clear'                  # only on win7
 alias nano='nano -w'
 alias more='less'
 alias lcsh='csh -l'
