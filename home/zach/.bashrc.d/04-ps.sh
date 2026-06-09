d8f7e0f834b17f41a305e9ebd612666f87cb95cc
Zachary Zhou <zchrzhou@gmail.com>
2026-06-09 18:17:04 +0800

    Update 04-ps.sh

--- home/zach/.bashrc.d/04-ps.sh
+++ home/zach/.bashrc.d/04-ps.sh
@@ -18,8 +18,6 @@ PS1=${TITLEBAR}${PS1}
 # emacs PS1 setting
 case $TERM in
    xterm* | emacs )
-     PS1='[\u@\h $(pwd | sed -e s!.*/zznix/*!/! | sed -e s!.*/home/zach!~!)]$ '
+     PS1='${USERHOST}\[\033[32m\]\u@\h $(pwd | sed -e s!.*/zznix/*!/! | sed -e s!.*/home/zach!~!)\[\033[0m\]$ '
      ;;
 esac
-
-
