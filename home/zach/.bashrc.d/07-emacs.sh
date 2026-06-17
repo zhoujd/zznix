a64a33b441786a22b73dcaed32fdd2ddfb24c1a6
Zachary Zhou <zchrzhou@gmail.com>
2026-06-17 22:46:08 +0800

    Update 07-emacs.sh

--- home/zach/.bashrc.d/07-emacs.sh
+++ home/zach/.bashrc.d/07-emacs.sh
@@ -1,11 +1,14 @@
 ### 07-emacs.sh
 
 me() {
-    if [[ "$1" == "--help" || "$1" == "--version" ]]; then
-        /zach/bin/me "$@"
-    else
-        printf "\033[?1049h"
-        /zach/bin/me "$@"
-        printf "\033[?1049l"
-    fi
+    case $1 in
+        --help|--version )
+            /zach/bin/me "$@"
+            ;;
+        * )
+            printf "\033[?1049h"
+            /zach/bin/me "$@"
+            printf "\033[?1049l"
+            ;;
+    esac
 }
