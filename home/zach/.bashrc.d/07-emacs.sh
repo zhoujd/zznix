39b0d8bdefbbe510d34629d8b26e1eabcd60b32f
Zachary Zhou <zchrzhou@gmail.com>
2026-06-17 23:11:38 +0800

    Update me files

--- home/zach/.bashrc.d/07-emacs.sh
+++ home/zach/.bashrc.d/07-emacs.sh
@@ -1,14 +1,1 @@
 ### 07-emacs.sh
-
-me() {
-    case $1 in
-        --help|--version )
-            /zach/bin/me "$@"
-            ;;
-        * )
-            printf "\033[?1049h"
-            /zach/bin/me "$@"
-            printf "\033[?1049l"
-            ;;
-    esac
-}
