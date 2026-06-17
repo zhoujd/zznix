22c1c4b3ee29aa25c8804b65877371cd82a87da9
Zachary Zhou <zchrzhou@gmail.com>
2026-06-17 22:35:41 +0800

    Update me in scripts

--- home/zach/.bashrc.d/06-function.sh
+++ home/zach/.bashrc.d/06-function.sh
@@ -1,14 +1,5 @@
 ## 06-zznix.sh
 
-microemacs() {
-    # 1. Switch Alacritty to the Alternate Screen Buffer
-    printf "\033[?1049h"
-    # 2. Run the editor binary (pass all file arguments)
-    /zach/bin/me "$@"
-    # 3. Upon exit, switch back to the Main Screen Buffer
-    printf "\033[?1049l"
-}
-
 cls() {
     printf "\033[2J\033[3J\033[H"
 }
--- home/zach/.bashrc.d/07-emacs.sh
+++ home/zach/.bashrc.d/07-emacs.sh
@@ -1,1 +1,11 @@
 ### 07-emacs.sh
+
+me() {
+    if [[ "$1" == "--help" || "$1" == "--version" ]]; then
+        /zach/bin/me "$@"
+    else
+        printf "\033[?1049h"
+        /zach/bin/me "$@"
+        printf "\033[?1049l"
+    fi
+}
