aa5a0208b2007809886852da798476e719903a94
Zachary Zhou <zchrzhou@gmail.com>
2026-09-25 23:50:32 +0800

    Update 03-path.sh

--- home/zach/.bashrc.d/03-path.sh
+++ home/zach/.bashrc.d/03-path.sh
@@ -2,16 +2,19 @@
 
 ADD_PATH=(
     /usr/local/bin
-    /zach/bin
     /zach/libexec
+    /zach/bin
     ~/.local/bin
     ~/.zach/bin
+    ~/zzemacs/libexec
+    ~/zzemacs/bin
 )
+
 for new_entry in ${ADD_PATH[@]} ; do
     if [ -d $new_entry ]; then
         case ":$PATH:" in
-            *":$new_entry:"*) :;;           # already there
-            *) PATH="$new_entry:$PATH";;    # or PATH="$PATH:$1"
+            *":$new_entry:"* ) ;;           # already there
+            * ) PATH="$new_entry:$PATH";;   # or PATH="$PATH:$1"
         esac
     fi
 done
